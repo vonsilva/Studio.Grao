3867a6ec8f63ef20665abd6cdb529d7bb22f2c12
vonslva <alves.joao978@icloud.com>
2020-01-29 12:39:06 -0300

    Modal com nome

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-$(document).ready(function() {
-    $('#modal').modal('show');
-})
+//Função definir cookie
+function definCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    };
 
-function getCookie(cname) {
+    
+//Função para pegar Cookie
+function pegarCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
@@ -21,31 +27,72 @@ function getCookie(cname) {
     };
 
 
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    };
-
-
+//função para botão 'Ok!'
 function salvar() {
     var nome_user = document.getElementById('nome_user').value;
-    setCookie('nome_user', nome_user, 30);
+    definCookie('nome_user', nome_user, 30);
     $('#modal').modal('hide');
-    console.log(nome_user);
+
+    sleep(800).then(() => {
+        var nome = pegarCookie('nome_user');
+
+        // Exibir modal com nome
+        document.getElementById("a_nome").innerHTML = nome;
+
+        // Exibir modal com nome
+        $('#modal_nome').modal('show');
+      })
 };
     
 
+// Função para salvar com enter
 function salvar_enter(e){
     e=e||window.event;
     var key = e.keyCode;
     if(key==13) {
         var nome_user = document.getElementById('nome_user').value;
-        setCookie('nome_user', nome_user, 30);
+        definCookie('nome_user', nome_user, 30);
         $('#modal').modal('hide');
-        return true;
+        
+        sleep(800).then(() => {
+            var nome = pegarCookie('nome_user');
+    
+            // Exibir modal com nome
+            document.getElementById("a_nome").innerHTML = nome;
+    
+            // Exibir modal com nome
+            $('#modal_nome').modal('show');
+          })
+
         }
     };
 
 
+function fechar() {
+    $('#modal_nome').modal('hide');
+}
+
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+
+var nome = pegarCookie('nome_user');
+console.log(nome);
+
+if (nome == '' || nome === 'Anônimo') {
+    definCookie('nome_user', 'Anônimo', 30);
+
+     // Exibir modal
+    $('#modal').modal('show');
+
+}
+else {
+    document.getElementById("a_nome").innerHTML = nome;
+
+    // Exibir modal com nome
+    $('#modal_nome').modal('show');
+}
+
+console.log(nome);
